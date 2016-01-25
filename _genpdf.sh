8c23d03f1f2cbb80ed981847f0d892e24e39aeec
aviaryan <avi.aryan123@gmail.com>
2016-01-25 11:58:28 +0530

    use parsed python files for making individual pdfs

--- _genpdf.sh
+++ _genpdf.sh
@@ -1,7 +1,10 @@
 mkdir _pdfs;
-for file in *.html.markdown
+#pandoc coffeescript.html.markdown -s -o _pdfs/coffeescript.pdf -V geometry:margin=1in --latex-engine=xelatex;
+#exit 1;
+for file in _temp/*.html.markdown
 do
-	langname=${file//\.html\.markdown/};
+	base=$(basename $file);
+	langname=${base//\.html\.markdown/};
 	echo "Making PDF $langname";
 	pandoc $file -s -o _pdfs/$langname.pdf -V geometry:margin=1in --latex-engine=xelatex
 done

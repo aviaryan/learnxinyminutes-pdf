e13bf947d4f5848b04370f3cdbfa97cb2de114d3
Norwid Behrnd <nbehrnd@yahoo.com>
2022-05-25 14:52:03 +0200

    substitute tabulator by explicit spaces, brief description

--- _genpdf.sh
+++ _genpdf.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Script creates individual .pdf for each entry of learnxinyminutes.
+
 mkdir _pdfs;
 #pandoc coffeescript.html.markdown -s -o _pdfs/coffeescript.pdf -V geometry:margin=1in --latex-engine=xelatex;
 #exit 1;
 for file in _temp/*.html.markdown
 do
-	base=$(basename "$file");
-	langname=${base//\.html\.markdown/};
-	echo "Making PDF $langname";
-	pandoc "$file" -s -o _pdfs/"$langname".pdf -V geometry:margin=1in --pdf-engine=xelatex
+    base=$(basename "$file");
+    langname=${base//\.html\.markdown/};
+    echo "Making PDF $langname";
+    pandoc "$file" -s -o _pdfs/"$langname".pdf -V geometry:margin=1in --pdf-engine=xelatex
 done
 
 mv _pdfs/c++.pdf _pdfs/cpp.pdf;

b9523e2530b85e0ea8643cd6f17ae257b56ff298
Norwid Behrnd <nbehrnd@yahoo.com>
2022-05-25 14:47:23 +0200

    account for suggestions by shellcheck
    Assuming the script aims for BASH, the syntax was checked with
    shellcheck[1] (version 0.8.0) and adjusted accordingly.
    
    [1] https://www.shellcheck.net/

--- _genpdf.sh
+++ _genpdf.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 mkdir _pdfs;
 #pandoc coffeescript.html.markdown -s -o _pdfs/coffeescript.pdf -V geometry:margin=1in --latex-engine=xelatex;
 #exit 1;
 for file in _temp/*.html.markdown
 do
-	base=$(basename $file);
+	base=$(basename "$file");
 	langname=${base//\.html\.markdown/};
 	echo "Making PDF $langname";
-	pandoc $file -s -o _pdfs/$langname.pdf -V geometry:margin=1in --pdf-engine=xelatex
+	pandoc "$file" -s -o _pdfs/"$langname".pdf -V geometry:margin=1in --pdf-engine=xelatex
 done
 
 mv _pdfs/c++.pdf _pdfs/cpp.pdf;
